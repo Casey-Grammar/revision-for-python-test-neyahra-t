29ed97815665c2b76ccc8596efdbc8bddcb7149d
neyahra-t <s39937@caseygrammar.vic.edu.au>
2026-03-25 21:41:25 +1100

    Update Task11.py

--- Task11.py
+++ Task11.py
@@ -4,21 +4,16 @@
 
 def main():
     #Fix the code starting here
-    drink = input("WHAT DRINK DO YOU WANT TO ORDER?)
+    drink = input('WHAT DRINK DO YOU WANT TO ORDER?')
     if drink.islower():
-     print("I DIDN'T HEAR YOUR ORDER!"
-      elif drink.isupper():
+     print("I DIDN'T HEAR YOUR ORDER!")
+    elif drink.isupper():
       many = input("HOW MANY? ")
-    coffee scoops = many * 4
+    coffee_scoops = many * 4
     print(f'{many} {drink}S COMING RIGHT UP!')
-    print(f'The barrister needs {coffee scoops} scoops of coffee in the coffee making machine')
+    print(f'The barrister needs {coffee_scoops} scoops of coffee in the coffee making machine')
     
 
 # End of code to be fixed
-
-
-
-
-
 if __name__ == '__main__':
     main()

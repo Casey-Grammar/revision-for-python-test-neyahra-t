1604b9a76f835933c2efe8874e417d0eb91fca26
neyahra-t <s39937@caseygrammar.vic.edu.au>
2026-03-26 01:29:01 +0000

    Commit tasks

--- Task2.py
+++ Task2.py
@@ -1,9 +1,10 @@
 # Task 2: James Bond
 def main():
     #Write your code for input here
-    
-
-
+    fname = input('What is your first name? ')
+    lname = input('What is your last name? ')
+    message = (f'My name is {lname}, {fname} {lname}')
+    print(message)
     # End of your code for input here
 
 if __name__ == '__main__':
